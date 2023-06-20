e5d272b0c491053eb5018090facd2c51e0ad21a1
azuressu <martina0130@naver.com>
2023-06-20 15:35:59 +0900

    RestTemplate GET 요청

--- src/main/java/com/sparta/springresttemplateclient/controller/RestTemplateController.java
+++ src/main/java/com/sparta/springresttemplateclient/controller/RestTemplateController.java
@@ -13,14 +13,27 @@ import java.util.List;
 @RequestMapping("/api/client")
 public class RestTemplateController {
 
+
     private final RestTemplateService restTemplateService;
 
     public RestTemplateController(RestTemplateService restTemplateService) {
         this.restTemplateService = restTemplateService;
     }
 
+    // api/client/get-call-obj를 get 방식으로 호출하면
+    // 클라이언트 입장 서버의 Controller로 옴
+    // 그리고 queryString으로 받아온 것을 service로 보냄
+    // Service로 넘어와서 URL을 만듦 (방향이 서버 입장의 서버임)
+    // 그리고 API path를 줌 -> /api/server/get-call/obj & 그리고 queryString을 붙임
+    // 그 다음 restTemplate을 사용해서 호출함
+    // 호출을 하면 server입장의 서버로 넘어감
+    // get-call-obj로 가서 service로 넘어가서 해당 메소드를 실행한 다음 결과값을 다시
+    // client 서버의 service로 돌아옴
+    // 그래서 그 값이 ResponseEntity로 들어오게 됨 (ItemDto 타입으로 받음)
+    // 마무리로 getBody를 하면, responseEntity 안에 들어있는 ItemDto 값을 반환할 수 있음
+
     @GetMapping("/get-call-obj")
-    public ItemDto getCallObject(String query) {
+    public ItemDto getCallObject(String query) { // @RequestParam 어노테이션 생략 가능
         return restTemplateService.getCallObject(query);
     }
 
--- src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -1,22 +1,71 @@
 package com.sparta.springresttemplateclient.service;
 
-
 import com.sparta.springresttemplateclient.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 public class RestTemplateService {
 
+    // Spring boot에서 만들어진 것을 주입받아서 사용할 것
+    private final RestTemplate restTemplate;
+
+    public RestTemplateService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build(); // build()는 restTemplate를 반환함
+    }
+
     public ItemDto getCallObject(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder // URI라는 클래스 객체를 만들 수 있음 (=우리가 요청할 URL을 만든다)
+                .fromUriString("http://localhost:7070") // 서버 입장의 서버에다 보낼 준비를 하는 것
+                .path("/api/server/get-call-obj")       
+                .queryParam("query", query)       // requestparam 방식으로 되어 있으므로 ? 뒤에 값을 받아옴 (검색어를 보냄) (Controller에서 받아옴)
+                .encode().build().toUri();
+
+        log.info("uri = " + uri);
+
+        // 요청을 보냈을 때, 데이터를 받음 (지금 Item을 물어봄. Item에 대한 정보를 ItemDto로 받을 것임)
+        // getForEntity는 get 방식으로 해당 URI 서버에 요청을 보냄
+        // 두 번째 parameter로 우리가 그 서버 입장의 서버, 요청을 한 그 서버에서 넘어오는 데이터를 받아줄 클래스 타입을 지정하면 자동으로 역직렬화가 되어서 객체 형태로 담김
+        // 받는 것은 ResponseEntity로 받을 수 있음 (ItemDto 타입으로)
+        ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+
+        // statuscode 서버 쪽에서도 날아옴 (성공하면 200이 날라올 것)
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> getCallList() {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070") //http://locaohost:7070
+                .path("/api/server/get-call-list")
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        // 여기서는 그냥 String으로 받음
+        // 위에서는 Json이 하나였는데, 여기서는 복합적이기 때문에 그냥 String으로 받은 다음
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+        log.info("Body = " + responseEntity.getBody());
+
+        // 여기서 다시 반환함 (json으로)
+        return fromJSONtoItems(responseEntity.getBody());
     }
 
     public ItemDto postCall(String query) {
@@ -26,4 +75,36 @@ public class RestTemplateService {
     public List<ItemDto> exchangeCall(String token) {
         return null;
     }
+
+/* {
+"items": {
+    {"title":"Mac", "price":3888000},
+    {"title":"iPad", "price":1230000},
+    {"title":"iPhone", "price":1550000},
+    {"title":"Watch", "price":450000},
+    {"title":"AirPods", "price":350000}
+  }
+} */
+
+    public List<ItemDto> fromJSONtoItems(String responseEntity) {
+        // 중첩 JSON의 형태로 넘어옴 (이 데이터를 우선 String으로 받아오게 됨)
+        JSONObject jsonObject = new JSONObject(responseEntity);
+        // getJSONArray - key값으로 items를 줌 (그 값이 items 임 !)
+        // 그러면 items의 내부의 값들만 저장되게 됨
+        JSONArray items = jsonObject.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Object item: items) {
+            // items 내부의 값들 하나씩 (위의 items 안에서 중괄호 한 줄 씩)
+            ItemDto itemDto = new ItemDto((JSONObject) item);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
+    }
 }
+
+/* 우리가 Object로 받아 올 때는 그냥 변환할 class 타입을 그냥 줬었는데,
+* 배열 형식으로 넘어올 경우 즉, 중첩 JSON 형식으로 넘어올 때는 그냥 String.class 타입으로 그 데이터를 한 번에 받음
+* 그러면 String으로 쭉 넘어올 것임.
+* 그러면 Json을 도와주는 라이브러리를 사용해서 JSONObject JSONArray를 사용해서 String으로 되어있는 중첩 JSON을 조작함*/
