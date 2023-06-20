8485690565ee5aec825cbb1c253abb2177a75995
azuressu <martina0130@naver.com>
2023-06-20 17:49:21 +0900

    Naver Open API 구현

---
+++ src/main/java/com/sparta/springresttemplateclient/naver/controller/NaverApiController.java
@@ -0,0 +1,28 @@
+package com.sparta.springresttemplateclient.naver.controller;
+
+import com.sparta.springresttemplateclient.naver.dto.ItemDto;
+import com.sparta.springresttemplateclient.naver.service.NaverApiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    public NaverApiController(NaverApiService naverApiService) {
+        this.naverApiService = naverApiService;
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItem(@RequestParam String query) { // key값은 query
+        return naverApiService.searchItems(query);
+    }
+
+
+}
